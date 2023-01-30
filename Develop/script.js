8cd08004d5c6f994fd09e00a856840d50284d098
Melissa Vaughn <119278059+Meljska-Fawn@users.noreply.github.com>
2023-01-30 00:31:28 -0800

    Added js, css, and html files

---
+++ Develop/script.js
@@ -0,0 +1,57 @@
+let timeDisplayEl = $('#currentDay');
+
+var currentDate = dayjs().format('MMM DD, YYYY');
+timeDisplayEl.text(currentDate);
+
+$(document).ready(function () {
+
+  $(".saveBtn").on("click", function () {
+    let text = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  })
+
+  function timeTracker() {
+    let now = moment();
+    let currentTime = now.hour();
+    console.log(currentTime);
+  
+
+    $(".time-block").each(function () {
+      let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+      if (timeBlock < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+      else if (timeBlock === currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    })
+  }
+
+
+$("#hour-8 .description").val(localStorage.getItem("hour8"));
+$("#hour-9 .description").val(localStorage.getItem("hour9"));
+$("#hour-10 .description").val(localStorage.getItem("hour10"));
+$("#hour-11 .description").val(localStorage.getItem("hour11"));
+$("#hour-12 .description").val(localStorage.getItem("hour12"));
+$("#hour-13 .description").val(localStorage.getItem("hour13"));
+$("#hour-14 .description").val(localStorage.getItem("hour14"));
+$("#hour-15 .description").val(localStorage.getItem("hour15"));
+$("#hour-16 .description").val(localStorage.getItem("hour16"));
+$("#hour-17 .description").val(localStorage.getItem("hour17"));
+
+timeTracker();
+
+})
+
